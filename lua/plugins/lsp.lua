cafd7f9590586fa8ddbec312695f94857d7c1466
Meth Munindradasa <methsndr@gmail.com>
2025-01-24 21:36:27 +1100

    Add lspconfig

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {}
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      ensure_installed = {
+        "lua_ls"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lconf = require("lspconfig")
+
+      lconf.lua_ls.setup({
+        settings = {
+          Lua = {
+            runtime = {version = "LuaJIT"}
+          }
+        }
+      })
+    end
+  }
+}
