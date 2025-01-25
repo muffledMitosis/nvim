894033f3c13b47cc9b2eb17f0a793634bdfabcf1
Meth Munindradasa <methsndr@gmail.com>
2025-01-26 10:21:18 +1100

    Add some new colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,7 +5,26 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+--      vim.cmd([[colorscheme tokyonight]])
     end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1001,
+    opts = {},
+    config = function()
+--      vim.cmd([[colorscheme gruvbox]])
+    end,
+  },
+  {
+    "shaunsingh/moonlight.nvim",
+    lazy = false,
+    priority = 1002,
+    opts = {},
+    config = function()
+      vim.cmd([[colorscheme moonlight]])
+    end,
+
   }
 }
