4a18a3f1a1b32b838d688c3b74cee5e5c34b3537
Meth Munindradasa <methsndr@gmail.com>
2025-01-26 20:36:06 +1100

    The only colorscheme that works with treesitter rn

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,30 +1,9 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
---      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-  {
     "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1001,
     opts = {},
     config = function()
---      vim.cmd([[colorscheme gruvbox]])
+      vim.cmd([[colorscheme gruvbox]])
     end,
   },
-  {
-    "shaunsingh/moonlight.nvim",
-    lazy = false,
-    priority = 1002,
-    opts = {},
-    config = function()
-      vim.cmd([[colorscheme moonlight]])
-    end,
-
-  }
 }
