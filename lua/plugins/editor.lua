734873ad9f515fc96698272cfd58d5d5e02267da
Meth Munindradasa <methsndr@gmail.com>
2025-01-27 22:09:26 +1100

    Add neo-tree

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -59,5 +59,15 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {}
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
   }
 }
