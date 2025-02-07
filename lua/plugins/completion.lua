fe89cc11a2f6aa13388a12494e90a3b10b3b7d40
Meth Munindradasa <methsndr@gmail.com>
2025-02-07 21:28:52 +1100

    Add some snippets

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,7 +7,13 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        init = function ()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end
+      },
       "saadparwaiz1/cmp_luasnip",
     },
     opts = function()
