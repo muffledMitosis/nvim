3e3b99c0a2e933a92b3ce497196acf60a12109df
Meth Munindradasa <methsndr@gmail.com>
2025-05-31 12:01:49 +1000

    Make jsx work and autopairing. Treesitter install.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -2,7 +2,13 @@ return {
   {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+      { 'rafamadriz/friendly-snippets' },
+      {
+        'L3MON4D3/LuaSnip',
+        version = 'v2.*',
+      }
+    },
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
@@ -37,6 +43,8 @@ return {
       -- (Default) Only show the documentation popup when manually triggered
       completion = { documentation = { auto_show = false } },
 
+      snippets = { preset = 'luasnip' },
+
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
@@ -51,5 +59,89 @@ return {
       fuzzy = { implementation = "prefer_rust_with_warning" }
     },
     opts_extend = { "sources.default" }
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    dependencies = {
+      {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+      },
+    },
+    opts = {}
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function ()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = {
+          'arduino',
+          'asm',
+          'bash',
+          'c',
+          'cmake',
+          'comment',
+          'cpp',
+          'css',
+          'csv',
+          'cuda',
+          'desktop',
+          'devicetree',
+          'diff',
+          'disassembly',
+          'dockerfile',
+          'doxygen',
+          'git_config',
+          'git_rebase',
+          'gitattributes',
+          'gitignore',
+          'glsl',
+          'go',
+          'hlsl',
+          'html',
+          'htmldjango',
+          'http',
+          'ini',
+          'javascript',
+          'jq',
+          'jsdoc',
+          'json',
+          'latex',
+          'linkerscript',
+          'llvm',
+          'lua',
+          'nasm',
+          'nginx',
+          'nix',
+          'ocaml',
+          'powershell',
+          'printf',
+          'python',
+          'regex',
+          'rust',
+          'scss',
+          'sql',
+          'ssh_config',
+          'tsx',
+          'typescript',
+          'verilog',
+          'vhdl',
+        },
+
+        sync_install = false,
+        auto_install = true,
+
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        }
+      }
+    end
   }
 }
